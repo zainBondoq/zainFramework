6463e7e41e163d8145d3fa4548efdd20e3952ef5
zainbondoq <zainbondoq@gmail.com>
2024-07-27 02:31:21 +0300

    user registration

--- bootstrap.php
+++ bootstrap.php
@@ -1,7 +1,8 @@
 <?php
   // Load Config
   require_once 'config/config.php';
-
+  // Load Helpers
+  require_once '../app/helpers/url_helper';
   // Autoload Core Libraries
   spl_autoload_register(function($className){
     require_once 'libraries/' . $className . '.php';
