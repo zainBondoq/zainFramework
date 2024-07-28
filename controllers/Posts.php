67c72219db96af5a290a39a1b9da336f684be9db
zainbondoq <zainbondoq@gmail.com>
2024-07-29 00:38:51 +0300

    post model and disply

--- controllers/Posts.php
+++ controllers/Posts.php
@@ -5,12 +5,16 @@ public function __construct() {
     if(!isLoggedIn()){
         redirect('/users/login');
     }
+    $this->postModel = $this->model('Post');
 }
 
 
 
     public function index(){
-        $data = [];
+        $posts = $this->postModel->getPosts();
+        $data = [
+            'posts' => $posts
+        ];
 
         $this->view('posts/index' , $data);
     }
--- models/post.php
+++ models/post.php
@@ -1,14 +1,25 @@
 <?php
-
-class Post {
+  class Post {
     private $db;
 
-    public function __construct() {
-        $this->db = new Database;
+    public function __construct(){
+      $this->db = new Database;
     }
 
-    public function getPosts() {
-        $this->db->query("SELECT * FROM posts"); // Make sure 'posts' is the correct table name
-        return $this->db->resultSet();
+    public function getPosts(){
+      $this->db->query('SELECT *,
+                        posts.id as postId,
+                        users.id as userId,
+                        posts.created_at as postCreated,
+                        users.created_at as userCreated
+                        FROM posts
+                        INNER JOIN users
+                        ON posts.user_id = users.id
+                        ORDER BY posts.created_at DESC
+                        ');
+
+      $results = $this->db->resultSet();
+
+      return $results;
     }
-}
+  }
